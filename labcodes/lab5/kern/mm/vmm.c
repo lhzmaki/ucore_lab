e3c3598b5132066b6d38d19d721759df64c08d97
os_lab_user <you@example.com>
2015-04-30 20:58:55 +0800

    add comment

--- labcodes/lab5/kern/mm/vmm.c
+++ labcodes/lab5/kern/mm/vmm.c
@@ -434,7 +434,7 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     ret = -E_NO_MEM;
 
     pte_t *ptep=NULL;
-    /*LAB3 EXERCISE 1: YOUR CODE
+    /*LAB3 EXERCISE 1: 2012011327
     * Maybe you want help comment, BELOW comments can help you finish the code
     *
     * Some Useful MACROs and DEFINEs, you can use them in below implementation.
@@ -451,15 +451,15 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
     *   mm->pgdir : the PDT of these vma
     *
     */
-#if 0
-    /*LAB3 EXERCISE 1: YOUR CODE*/
-    ptep = ???              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
+
+    /*LAB3 EXERCISE 1: 2012011327*/
+    ptep = get_pte(mm->pgdir, addr, 1);              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
     if (*ptep == 0) {
                             //(2) if the phy addr isn't exist, then alloc a page & map the phy addr with logical addr
-
+    	pgdir_alloc_page(mm->pgdir, addr, perm);
     }
     else {
-    /*LAB3 EXERCISE 2: YOUR CODE
+    /*LAB3 EXERCISE 2: 2012011327
     * Now we think this pte is a  swap entry, we should load data from disk to a page with phy addr,
     * and map the phy addr with logical addr, trigger swap manager to record the access situation of this page.
     *
@@ -478,21 +478,33 @@ do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr) {
 		     This method could be used to implement the Copy on Write (COW) thchnology(a fast fork process method).
 		  2) *ptep & PTE_P == 0 & but *ptep!=0, it means this pte is a  swap entry.
 		     We should add the LAB3's results here.
-     */
-        if(swap_init_ok) {
-            struct Page *page=NULL;
-                                    //(1）According to the mm AND addr, try to load the content of right disk page
+     */ 
+        struct Page *page=NULL;
+        cprintf("do pgfault: ptep %x, pte %x\n",ptep, *ptep);
+        if (*ptep & PTE_P == 1){
+             panic("error write a non-writable pte");
+        }
+        else{
+            if(swap_init_ok) {
+                                       
+            						//(1）According to the mm AND addr, try to load the content of right disk page
                                     //    into the memory which page managed.
+                if ((ret = swap_in(mm, addr, &page)) != 0) {
+                    cprintf("swap_in in do_pgfault failed\n");
+                    goto failed;
+                }
                                     //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
                                     //(3) make the page swappable.
-                                    //(4) [NOTICE]: you myabe need to update your lab3's implementation for LAB5's normal execution.
+            }
+            else {
+                cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+                goto failed;
+            }
         }
-        else {
-            cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
-            goto failed;
-        }
-   }
-#endif
+        page_insert(mm->pgdir, page, addr, perm);
+        swap_map_swappable(mm, addr, page, 1);
+    }
+
    ret = 0;
 failed:
     return ret;
---
+++ labcodes/lab5/report.md
@@ -0,0 +1,35 @@
+```
+练习一：
+按照注释给tf中的参数赋值，将cs，ds，es，ss设置为用户态，则可进行内核态到用户态的切换
+
+proc_run函数会载入下一个进程的kstack和cr3，并执行switch_to，保存当前进程的eip，push下一个进程的eip，并用ret跳转到eip执行。而在load_icode中我们已经设置了eip为用户程序入口，所以switch_to返回的即为用户程序入口，又我们设置了cs，ds等为用户态对应值，之后就可以开始执行用户程序了
+```
+
+```
+练习二：
+在进程fork时，为共用相同内存块，可将子进程的cr3设置为和父进程相同，同时为了防止对某共享资源的修改，可将该资源对应的页设置为只读，当需要修改时，就会发生page_fault，在处理该page_fault时判断时候为COW，若是则调用copy_range进行拷贝，并将PTE_W设置为1
+```
+
+```
+练习三：
+do_fork:
+产生一个子进程，并将唤醒子进程（即将其状态设为PROC_RUNNABLE）
+do_execve：
+加载一个可执行程序到进程中，但并未改变进程状态
+do_wait：
+检查当前进程的子进程，若有子进程，则将当前进程状态设为PROC_SLEEPING，并将子进程中状态为PROC_ZOMBIE的进程释放，若无子进程，则直接结束这个进程释放资源
+do_exit：
+将当前进程状态改为PROC_ZOMBIE，若当前进程的父进程的wait_state为WT_CHILD，则将父进程唤醒，若此进程含有子进程，则将子进程交由内核线程initproc处理
+
+状态图：
+			wakeup_proc                           do_wait
+PROC_UNINIT_--------------------------->                    ----------------------> 
+					PROC_RUNNABLE <---------------------PROC_SLEEPING                         
+						|		do_exit		|			
+						|					|
+						|do_exit				|
+						|					|
+						|			do_exit		|		
+					PROC_ZOMBIE <--------------------------------------|		
+                            
+```
